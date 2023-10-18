cce31e45488e17ee100b99ea0eb3fd4574c20cb9
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-10-18 21:53:26 +0300

    Initial Commit

---
+++ webViewSwiftUI/webViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  webViewSwiftUIApp.swift
+//  webViewSwiftUI
+//
+//  Created by Роман on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct webViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
