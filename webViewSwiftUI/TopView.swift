4b791bfa2567fc6b6680723d182b9b55f5c7f39e
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-10-18 22:52:27 +0300

    get tapabr

---
+++ webViewSwiftUI/TopView.swift
@@ -0,0 +1,18 @@
+//
+//  TopView.swift
+//  webViewSwiftUI
+//
+//  Created by Роман on 18.10.2023.
+//
+
+import SwiftUI
+
+struct TopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopView()
+}
