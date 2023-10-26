94cd28f5e328341eac7b993607d06a2e89882ed1
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-10-26 22:56:26 +0300

    get LaunchScreen

---
+++ webViewSwiftUI/LaunchScreen/LaunchScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreenView.swift
+//  webViewSwiftUI
+//
+//  Created by Роман on 26.10.2023.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
