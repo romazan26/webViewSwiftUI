a8d20725f87c6e01ddc0c2292eaff8f2ffe92abd
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-10-31 00:01:02 +0300

    new CurrencyPair View

---
+++ webViewSwiftUI/Currency pair/CurrencyPairView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyPairView.swift
+//  webViewSwiftUI
+//
+//  Created by Роман on 30.10.2023.
+//
+
+import SwiftUI
+
+struct CurrencyPairView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrencyPairView()
+}
